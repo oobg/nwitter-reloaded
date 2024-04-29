a2d10fb114e04942c85d61fd1784dc874e5ebe17
yoons <yoonseok.bae98@dotincorp.com>
2024-04-29 16:48:42 +0900

    Feat: Firebase init

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAn4LohLcrFpem_kVF3iCgRMXbz3u2AfAU",
+    authDomain: "nwitter-reloaded-14ce0.firebaseapp.com",
+    projectId: "nwitter-reloaded-14ce0",
+    storageBucket: "nwitter-reloaded-14ce0.appspot.com",
+    messagingSenderId: "342192028814",
+    appId: "1:342192028814:web:82a17a83202093b6bf9ddf"
+};
+
+const app = initializeApp(firebaseConfig);
