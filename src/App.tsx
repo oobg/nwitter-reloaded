3c960d93114a58164858912ffe2de4294dbe3d62
yoons <yoonseok.bae98@dotincorp.com>
2024-04-29 16:27:31 +0900

    Feat: Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,42 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login.tsx";
+import CreateAccount from "./routes/create-account.tsx";
+
+const router = createBrowserRouter([
+  {
+    "path": "/",
+    element: <Layout />,
+    children: [
+      {
+        "path": "/",
+        "element": <Home />,
+      },
+      {
+        "path": "/profile",
+        "element": <Profile />,
+      }
+    ]
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "create-account",
+    element: <CreateAccount />,
+  }
+])
+
 function App() {
 
-  return <></>;
+  return (
+      <>
+        <RouterProvider router={router} />
+      </>
+  );
 }
 
-export default App
+export default App;
